03aec9d698a8027dcf4260f8ee354aaf11fe629c
Paula Moutafian <pamoutaf@mi-r3-p7.s19.be>
2021-11-25 18:43:34 +0100

    commit

--- ft_keys.c
+++ ft_keys.c
@@ -6,7 +6,7 @@
 /*   By: pamoutaf <pamoutaf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/10 15:07:54 by pamoutaf          #+#    #+#             */
-/*   Updated: 2021/11/25 18:01:45 by pamoutaf         ###   ########.fr       */
+/*   Updated: 2021/11/25 18:40:18 by pamoutaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -62,29 +62,25 @@ void move_vertical(t_global *global, t_map_data *data, int direction)
 	}
 }
 
-t_pos move_horizontal(t_global *global, t_map_data *data, int direction)
+void move_horizontal(t_global *global, t_map_data *data, int direction)
 {
-	int		x;
-	int		y;
 	t_pos	pos;
 	void	*mlx_destroy;
 	void	*img_destroy;
 
-	pos = png_to_win(global->mlx, global->map_data, global->win, global->img);
-	x = pos.x;
-	y = pos.y;
+	
 	//mlx_destroy = global->mlx;
 	//img_destroy = global->img->sprite_player;
 	printf("position Y : %i\n", pos.y);
 	printf("position X : %i\n", pos.x);
-	if (check_wall(data, x + direction, y))
+	if (check_wall(data, pos.x + direction, pos.y))
 	{
 		//mlx_put_image_to_window(global->mlx, global->win, global->img->sprite_empty, pos.x, pos.y);
 		pos.x = pos.x +(direction);
+		pos = png_to_win(global->mlx, global->map_data, global->win, global->img);
 		//mlx_destroy_image(mlx_destroy, img_destroy);
 		mlx_put_image_to_window(global->mlx, global->win, global->img->sprite_player, pos.x * 64, pos.y * 64);
 		printf("new position x: %i\n", pos.x);
 		printf("----------------------------------\n");
 	}
-	return (pos);
 }
--- solong.h
+++ solong.h
@@ -6,7 +6,7 @@
 /*   By: pamoutaf <pamoutaf@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/06 17:17:06 by pamoutaf          #+#    #+#             */
-/*   Updated: 2021/11/25 16:10:07 by pamoutaf         ###   ########.fr       */
+/*   Updated: 2021/11/25 18:33:02 by pamoutaf         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ t_pos		png_to_win(void *mlx_ptr, t_map_data *data, void *win, t_sprite *sprite);
 int			move_left(t_pos *pos, int direction);
 int			check_wall(t_map_data *data, int x, int y);
 void		move_vertical(t_global *global, t_map_data *data, int direction);
-t_pos		move_horizontal(t_global *global, t_map_data *data, int direction);
+void		move_horizontal(t_global *global, t_map_data *data, int direction);
 
 
 #endif
